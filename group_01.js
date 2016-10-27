ae8c50efd8221777227f98fe8e05bd87cc17e2aa
Luke Schlangen <lukeschlangen@gmail.com>
2016-10-27 12:59:56 -0500

    Basic bonus calculation

--- group_01.js
+++ group_01.js
@@ -6,3 +6,36 @@ var robert = ["Robert", "26835", "66000", 1];
 var mayella = ["Mayella", "89068", "35000", 2];
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
+
+for(var i =0; i < employees.length; i++){
+  var employee = employees[i];
+  console.log(bonusArrayBuilder(employee));
+}
+
+function bonusArrayBuilder(employee){
+  var employeeName = employee[0];
+  var employeeNumber = employee[1];
+  var employeeSalary = employee[2];
+  var employeePerformace = employee[3];
+
+  var bonusPercentage = basePerformanceBonusCalulator(employeePerformace);
+
+
+  return [employeeName, bonusPercentage];
+}
+
+function basePerformanceBonusCalulator(employeePerformace) {
+  var baseBonusPercentage = 0;
+  switch (employeePerformace) {
+    case 5:
+      baseBonusPercentage = 0.1;
+      break;
+    case 4:
+      baseBonusPercentage = 0.06;
+      break;
+    case 3:
+      baseBonusPercentage = 0.04;
+      break;
+  }
+  return baseBonusPercentage;
+}
